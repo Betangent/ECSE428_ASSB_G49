f984e02642c1e1f50b31377cad37a003148d3e13
Betangent <rwa_edwin@hotmail.com>
2021-11-07 00:58:35 -0400

    Refactored base js file

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-var express = require("express");
-var app = express();
+// Imports
+let express = require("express");
+let app = express();
+// Properties
+let port = 8080;
 
-app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"])
+// MAIN FUNCTION FOR THE PURPOSE OF THIS ASSIGNMENT
+// POST request at /rate
+app.post("/rate", (req, res, next) => {
+    console.log("Received POST request at /rate");
+    console.log(req.json);
+    res.status(501).json("Function is to be implemented after tests are created.");
 });
 
-app.listen(3000, () => {
+// GET request at /about
+app.get("/about", (req, res, next) => {
+    res.json([
+        "ECSE428",
+        "FALL2021",
+        "ASSIGNMENT B",
+        "MCGILL",
+        "ASSIGNMENT COMPLETED BY SOLELY EDWIN PAN"
+    ])
+});
+
+app.listen(port, () => {
     console.log("Server running on port 3000")
 });
+
+// For testing
+module.exports = app;

a834b034d4905046082049aecf914cdb4210e882
Betangent <rwa_edwin@hotmail.com>
2021-11-07 01:21:42 -0400

    Bugfix first test now working.

--- test/LengthTests.js
+++ test/LengthTests.js
@@ -12,12 +12,12 @@ describe("Test on POST request on /rate focusing on length properties", () =>{
     describe("POST /rate with -1 mm length ", () =>{
         it("it should yield a Bad Request response status", (done) => {
             let envelopeProperties = {
-                length = -1,
-                length_unit = "mm",
-                width = 125,
-                width_unit = "mm",
-                weight = 15,
-                weight_unit = "g"
+                length: -1,
+                length_unit: "mm",
+                width: 125,
+                width_unit: "mm",
+                weight: 15,
+                weight_unit: "g"
             }
             chai.request(server)
             .post("/rate")

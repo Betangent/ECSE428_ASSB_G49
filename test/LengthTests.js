1ff1c3dd91c6835aa3bc87956d6566e488fb8308
Betangent <rwa_edwin@hotmail.com>
2021-11-07 18:15:17 -0500

    Refactor length test to reduce copy pasted code

--- test/LengthTests.js
+++ test/LengthTests.js
@@ -6,26 +6,38 @@ let server = require("../index.js");
 let should = chai.should();
 
 chai.use(chaiHttp);
+
+// Test Template for Length Tests
+function testRateWithVaryingLengths(l, l_u, rstatus, rrate = null){
+    let envelopeProperties = {
+        length : l, 
+        length_unit : l_u,
+        width: 125,
+        width_unit: "mm",
+        weight: 15,
+        weight_unit: "g"
+    };
+    chai.request(server)
+    .post("/rate")
+    .send(envelopeProperties)
+    .end((err,res) => {
+        // Assert status
+        res.should.have.status(rstatus)
+        // Assert rate, if needed
+        if(rrate !== null){
+            res.body.should.have.property("rate").equal(rrate);
+        }
+        // Indicate test is complete
+        done();
+    });
+}
+
 // Test Parent Block
 describe("Test /rate POST, varying sent length properties", () =>{
 
     describe("POST /rate with -1 mm length ", () =>{
         it("it should yield a Bad Request response status", (done) => {
-            let envelopeProperties = {
-                length: -1,
-                length_unit: "mm",
-                width: 125,
-                width_unit: "mm",
-                weight: 15,
-                weight_unit: "g"
-            }
-            chai.request(server)
-            .post("/rate")
-            .send(envelopeProperties)
-            .end((err,res) => {
-                res.should.have.status(400);
-                done();
-            });
+            testRateWithVaryingLengths(-1,"mm",400)
         });
     });
 

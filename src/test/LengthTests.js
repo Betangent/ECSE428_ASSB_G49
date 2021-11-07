9ad5690dc79c999fbd0824567b407e800fee5123
Betangent <rwa_edwin@hotmail.com>
2021-11-07 01:16:25 -0400

    Commit first test. First test not itself tested.

--- src/test/LengthTests.js
+++ src/test/LengthTests.js
@@ -2,3 +2,31 @@ process.env.NODE_ENV = 'test';
 
 let chai = require('chai');
 let chaiHttp = require('chai-http');
+let server = require("../index.js");
+let should = chai.should();
+
+chai.use(chaiHttp);
+// Test Parent Block
+describe("Test on POST request on /rate focusing on length properties", () =>{
+
+    describe("POST /rate with -1 mm length ", () =>{
+        it("it should yield a Bad Request response status", (done) => {
+            let envelopeProperties = {
+                length = -1,
+                length_unit = "mm",
+                width = 125,
+                width_unit = "mm",
+                weight = 15,
+                weight_unit = "g"
+            }
+            chai.request(server)
+            .post("/rate")
+            .send(envelopeProperties)
+            .end((err,res) => {
+                res.should.have.status(400);
+            });
+
+        });
+    });
+
+});
